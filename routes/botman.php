5e300223eaf15cb8e532ea1a4218d7dac04dc25c
04-user01 <04-user01@donnu.edu.ua>
2020-01-11 11:47:08 +0300

    2

--- routes/botman.php
+++ routes/botman.php
@@ -7,11 +7,28 @@ $botman = resolve('botman');
 
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
+
+    $question = Question::create('Do you need a database?')
+        ->fallback('Unable to create a new database')
+        ->callbackId('create_database')
+        ->addButtons([
+            Button::create('Of course')->value('1'),
+            Button::create('Hell no!')->value('2'),
+        ]);
+
+    $bot->reply($question);
+
 });
 $botman->hears('1', function ($bot) {
-    $bot->reply("Tell me more!");
+    $bot->reply("Tell me more!1");
     $bot->reply("And even more");
 });
+
+$botman->hears('2', function ($bot) {
+    $bot->reply("Tell me more!2");
+    $bot->reply("And even more");
+});
+
 $botman->hears('img', function ( $bot) {
     // Create attachment
     $attachment = new Image('https://botman.io/img/logo.png');
